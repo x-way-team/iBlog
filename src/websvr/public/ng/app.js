99719fbf7e36295f1bed1cb7edcaeb0de6fe2a72
winnie-zy <winnie.zy@outlook.com>
2015-01-01 00:57:02 +0800

    add logo

--- src/websvr/public/ng/app.js
+++ src/websvr/public/ng/app.js
@@ -7,11 +7,12 @@
 // Declare app level module which depends on filters, and services
 var myApp = angular.module('myApp', ['ngRoute', 
             'myApp.controllers', 'myApp.filters', 'myApp.services', 'myApp.directives']);
-myApp.config(['$routeProvider', function($routeProvider) {
+myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.when('/', { redirectTo: '/home' });
     $routeProvider.when('/home', { templateUrl: 'home.ejs', controller: 'MyCtrl2' });
     $routeProvider.when('/404', { templateUrl: '404.ejs', controller: 'MyCtrl2' });
     $routeProvider.otherwise({ redirectTo: '/404' });
+    $locationProvider.html5Mode(true);
 }]);
   
 myApp.run(['$rootScope', '$location', '$window', function ($rootScope, $location, $window) { 
